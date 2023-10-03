cf5417d36769144397ea93b9364e38774f033335
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2023-10-03 18:23:52 -0300

    Clase 4

--- main.cpp
+++ main.cpp
@@ -3,5 +3,54 @@
 using namespace std;
 
 int main() {
-    cout << "Hola mundo";
+    int edad = 0;
+    edad = 28;
+    const char letra = 'A';
+    int lista_edades[] = { 18, 10, 25};
+    cout <<  edad << endl;
+    cout << letra << endl;
+    cout << lista_edades[ 1 ];
 }
+
+
+/*
+Clase 03
+## Tipos de datos
+
+Bool(Boolean):
+
+- Valor lógico
+- Puede ser Verdadero o Falso
+- Tamaño 4 bytes
+
+Int(Integer)
+
+- Números enteros
+- Tamaño 5 bytes
+
+Float
+
+- Números decimales (punto flotante)
+- Tamaño: 4 bytes
+
+Double:
+
+- Números de punto flotante de doble precisión
+- Tamaño 8 bytes (4+4 = doble flotante)
+
+Char (Character): 
+
+- Carácter (ASCII)
+- Muestra el caracter o su numero en ASCII
+- Tamaño 4 bytes
+
+Por ejemplo: “Hola Mundo” o “65”
+
+También se tiene:
+
+Short Int
+
+Unsign Int
+
+Long Double
+*/
