de5f508623c37282086d19543d7e4a002c50fcb8
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2024-08-07 14:00:54 -0400

    Create funciones01.cpp

---
+++ funciones01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void PrintCuteMessage()
+{
+    cout << "Eres terrible pulento" << endl;
+    cout << "vo dale <3" << endl;
+}
+
+int main(){
+
+    PrintCuteMessage();
+
+    return 0;
+}
