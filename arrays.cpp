61d21ddd031c81378a29d4a9d122d73d5017b636
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2024-08-07 14:00:46 -0400

    Mas Clases

---
+++ arrays.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    string personajes[5] = { "Astrid", "Kali", "Lydia", "Oma", "Meredith" };
+
+    string Character = personajes[2];
+
+    cout << Character << endl;
+
+}
