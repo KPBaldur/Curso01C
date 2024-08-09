e79656b3ec0263f9c58456dec813c27025082e1a
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2024-08-09 15:16:15 -0400

    Update creaarchivos.cpp

--- CreaArchivos/creaarchivos.cpp
+++ CreaArchivos/creaarchivos.cpp
@@ -5,16 +5,46 @@ using namespace std;
 
 int main()
 {
+    
     ofstream MyFile("GameData.txt");
+    string PlayerName = "";
 
     if(MyFile.is_open())
     {
-        MyFile << "Hola Mundo!";
+        MyFile << "Hola Mundo!" << endl;
+        MyFile << "Mi nombre es" << endl;
 
-        for(int i = 0; i < 10; i++ )
-
-        MyFile << i << endl;
+        cout << "Introduce el nombre de tu Heroe" << endl;
+        cin >> PlayerName;
+        MyFile << PlayerName;
     }
+
+    MyFile.close();
+
+    ifstream MyFileRead("GameData.txt");
+    string line;
+    string NombreDelHeroe = "";
+    int Reglon = 0;
+
+    if(MyFileRead.is_open())
+    {
+        while(getline(MyFileRead,line))
+        {
+            
+            if(Reglon == 2){
+                NombreDelHeroe = line;
+            }
+
+            Reglon = Reglon + 1;
+        }
+    } 
+    else 
+    {
+    cout << "Problemas de lectura, revisa el antivirus" << endl;
+    }
+
+        cout << "Bienvenida a tu gran aventura! " << endl;
+        cout << NombreDelHeroe;
 
     return 0;
 }
