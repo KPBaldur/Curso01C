9621d313c7d85f028c9599d55cd2a68c48175a0d
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2024-08-08 22:32:36 -0400

    Create cicloWhile.cpp

---
+++ cicloWhile.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int contador = 1;
+
+    // Ciclo While que imprime los numeros del 1 al 5
+    while (contador <= 5) {
+        cout << contador << " ";
+        ++contador;
+
+    }
+
+    return 0;
+}
