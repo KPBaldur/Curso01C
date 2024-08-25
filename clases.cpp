9bcf67d8c4734f59edc0bf1812460c6552beaefd
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2024-08-24 22:34:59 -0400

    Clases y ejercicios

---
+++ clases.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+class Cat
+{
+    public:
+        string Name;
+
+        Cat()
+        {
+            Name = "Guapo";
+        }
+};
+
+int main()
+{
+
+
+    return 0;
+}
---
+++ ejercicio.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a = 10;
+    int b = a /2;
+    float d > b %2;
+    int c = d * 6;
+}
